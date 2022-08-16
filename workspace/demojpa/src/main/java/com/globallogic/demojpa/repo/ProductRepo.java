3cb2041af8f1870ca0cbbb59d5e21cadc3f9a9e8
shagungl <110555208+shagungl@users.noreply.github.com>
2022-08-16 14:31:16 +0530

    Delete workspace/demojpa directory

--- workspace/demojpa/src/main/java/com/globallogic/demojpa/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.globallogic.demojpa.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.globallogic.demojpa.entity.ProductEntity;
-
-public interface ProductRepo extends JpaRepository<ProductEntity, Integer> {
-
-}
